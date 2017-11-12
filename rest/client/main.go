fee6f28a38b2c1b48d2309fc8cbb977ef229210d
Loh Siu Yin <siuyin@beyondbroadcast.com>
2017-11-12 09:24:08 +0000

    Added REST examples.

---
+++ rest/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+//010 OMIT
+func main() {
+	fmt.Println("REST example")
+	v := url.Values{}
+	v.Set("a", "3")
+	v.Add("b", "4")
+	q := v.Encode()
+
+	resp, err := http.Get("http://localhost:8080/sum?" + q)
+	if err != nil {
+		log.Fatalf("could not GET from server: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("unable to read response body: %v", err)
+	}
+	fmt.Printf("the sum of 3 and 4 is: %s", string(body))
+}
+
+//020 OMIT
---
+++ rest/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+//030 OMIT
+func sumStr(as, bs string) (int, error) {
+	var (
+		a, b int
+		err  error
+	)
+	if a, err = strconv.Atoi(as); err != nil {
+		return 0, fmt.Errorf("a param bad: %v", err)
+	}
+	if b, err = strconv.Atoi(bs); err != nil {
+		fmt.Println(a)
+		return 0, fmt.Errorf("b param bad: %v", err)
+	}
+	return a + b, nil
+}
+
+//040 OMIT
+
+//010 OMIT
+func main() {
+	fmt.Println("REST server")
+	http.HandleFunc("/sum", func(w http.ResponseWriter, r *http.Request) {
+		aStr := r.FormValue("a")
+		bStr := r.FormValue("b")
+		sum, err := sumStr(aStr, bStr)
+		if err != nil {
+			fmt.Fprintf(w, "ERROR: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "%v", sum)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+//020 OMIT
