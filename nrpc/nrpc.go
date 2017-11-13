297e54f4b27fb85d78ec2fd3b2eb0778ef231be1
Loh Siu Yin <siuyin@beyondbroadcast.com>
2017-11-13 02:25:44 +0000

    Addded comment on empty struct.

--- nrpc/nrpc.go
+++ nrpc/nrpc.go
@@ -4,7 +4,7 @@ package nrpc
 type Args struct {
 	A, B int
 }
-type Arith int
+type Arith int // can be any base type. For saving space use struct{} -- empty struct.
 
 func (t *Arith) Sum(args *Args, reply *int) error {
 	*reply = args.A + args.B
